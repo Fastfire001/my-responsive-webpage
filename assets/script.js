33a713c54f16a5b0cf2dc54f3ad15108f6201036
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-03-25 15:16:37 +0200

    added some javascript

--- assets/script.js
+++ assets/script.js
@@ -6,43 +6,62 @@ function main() {
     document.querySelector('li.btn-pf-journal').addEventListener("click", showJournalPf);
 }
 
+function hideSelectedBtn() {
+
+    document.querySelector('li.btn-pf-all').classList.remove('selected');
+    document.querySelector('li.btn-pf-panoramas').classList.remove('selected');
+    document.querySelector('li.btn-pf-portraits').classList.remove('selected');
+    document.querySelector('li.btn-pf-macro').classList.remove('selected');
+    document.querySelector('li.btn-pf-journal').classList.remove('selected');
+}
+
 function showPanoramasPf() {
+    hideSelectedBtn();
     hideAllPf();
     var imgs = document.querySelectorAll('img.panoramas');
     for (var i = 0; i < imgs.length; i++){
         imgs[i].classList.remove('hide');
     }
+    this.classList.add('selected');
 }
 
 function showPortraitsPf() {
+    hideSelectedBtn();
     hideAllPf();
     var imgs = document.querySelectorAll('img.portraits');
     for (var i = 0; i < imgs.length; i++){
         imgs[i].classList.remove('hide');
     }
+    this.classList.add('selected');
 }
 
 function showMacroPf() {
+    hideSelectedBtn();
     hideAllPf();
     var imgs = document.querySelectorAll('img.macro');
     for (var i = 0; i < imgs.length; i++){
         imgs[i].classList.remove('hide');
     }
+    this.classList.add('selected');
 }
 
 function showJournalPf() {
+    hideSelectedBtn();
     hideAllPf();
     var imgs = document.querySelectorAll('img.journal');
     for (var i = 0; i < imgs.length; i++){
         imgs[i].classList.remove('hide');
     }
+    this.classList.add('selected');
 }
 
 function showAllPf() {
+    hideSelectedBtn();
     var imgs = document.querySelectorAll('img.img-pf');
     for (var i = 0; i < imgs.length; i++){
         imgs[i].classList.remove('hide');
     }
+    this.classList.add('selected');
 }
 
 function hideAllPf() {
